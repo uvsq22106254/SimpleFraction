68927734b71e6d05861da65479882656aaf90892
Nazim Bouskra <nazimbouskra@gmail.com>
2021-10-13 18:42:00 +0200

    Ajout du constructeur et la méthode toString à la classe Fraction et modification de la classe Main

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        Fraction uneVariable = new Fraction(5, 9);
-        System.out.print(uneVariable.toString());
+        Fraction uneFraction = new Fraction(1, 2);
+        System.out.print(uneFraction.toString());
     }
   }
